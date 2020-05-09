30f4832093fb4a0e401bbe53121877e7b0348a30
Mariscos1 <ramon.marquez3@yahoo.com>
2020-05-09 12:33:36 -0500

    Hello

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NB from './NavBar.js'
+import {BrowserRouter} from 'react-router-dom'
+import Main from './Main.js'
+import { Link } from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <p id = "frontTitle" ><Link to= '/'> Ramonji / ラモンジ </Link></p>
+        <Main/>
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+// <iframe scrolling = "no" src="https://docs.google.com/document/d/e/2PACX-1vToqcBb4fUNUfHtsCrsO_4Fx5lgIAeR6RODUIiVHE3WrnI82N24kfpZmFCuWb0SLj2tj44GgjDxj7b8/pub?embedded=true"></iframe>
